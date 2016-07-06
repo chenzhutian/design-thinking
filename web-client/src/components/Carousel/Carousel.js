29cd93aa535f827f2c0bfd927d8118180e8ec0fe
unHealthy <chenzhutian1992@gmail.com>
2016-07-06 18:59:54 +0800

    socket

--- web-client/src/components/Carousel/Carousel.js
+++ web-client/src/components/Carousel/Carousel.js
@@ -1,7 +1,12 @@
+import IO from 'socket.io-client';
+
 export default {
     ready() {
-
+        this.socket = new IO('/ALBUMN');
+        this.socket.emit('login', this.loginParams);
+        this.socket.on('moveSlides', this.setCurrentSlide);
     },
+    socket: null,
     data() {
         return {
             currentSlideIndex: 0,
@@ -9,6 +14,7 @@ export default {
             imagesCount: 0,
             carouselWraperStyle: { left: 0, width: 0 },
             imageStyle: {},
+            loginParams: { userType: 'parent', roomName: 'designThinking' },
         };
     },
     methods: {
@@ -17,9 +23,14 @@ export default {
             this.imagesCount = this.images.length;
             this.carouselWraperStyle.width = `${100 * this.imagesCount}%`;
         },
+        moveSlides(index) {
+            this.setCurrentSlide(index);
+            this.socket.emit('moveSlides', index);
+        },
         setCurrentSlide(index) {
             this.currentSlideIndex = index;
             this.carouselWraperStyle.left = `${-100 * this.currentSlideIndex}%`;
         },
+
     },
 };
