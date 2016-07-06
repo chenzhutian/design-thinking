9ec71d95353da8b318e670116ae3a1a095bbd51a
unHealthy <chenzhutian1992@gmail.com>
2016-07-06 16:34:39 +0800

    update image

--- web-client/src/components/App/App.js
+++ web-client/src/components/App/App.js
@@ -4,25 +4,29 @@ import 'material-design-lite/src/material-design-lite.scss';
 // components
 import Carousel from '../Carousel/Carousel.vue';
 
+import image1 from '../../assets/1.jpg';
+import image2 from '../../assets/2.jpg';
+import image3 from '../../assets/3.jpg';
+import image4 from '../../assets/4.jpg';
 
 export default {
     ready() {
         mdl.upgradeDom();
         const images = [
             {
-                img: 'http://acmedia365.com/wp-content/uploads/2016/02/201009101513590430.jpg',
+                img: image1,
                 msg: 'Craving for more than campus food? Fortunately, you have FooPar Beta!',
             },
             {
-                img: 'https://wallpaperscraft.com/image/salad_vegetables_greens_healthy_food_20779_1024x768.jpg',
+                img: image2,
                 msg: 'Try our catering service for a variety of snacks and refreshments for functions',
             },
             {
-                img: 'http://myhealthydish.com/wp-content/uploads/2015/07/healthy-foods-1024x768.jpg',
+                img: image3,
                 msg: 'Simply choose from our lunch selection to avoid queues in the canteens',
             },
             {
-                img: 'http://www.wallcoo.net/photography/sz189-at-the-Table/wallpapers/1024x768/foods-on-table-HV038_350A_wallcoo.com.jpg',
+                img: image4,
                 msg: "Anytime you feel like eating off-campus foods, don't hesitate to tell us.",
             },
         ];
