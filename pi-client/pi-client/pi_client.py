ad73117b9d7157bdd192f34ee0a78ded3d068606
unHealthy <chenzhutian1992@gmail.com>
2016-07-08 16:50:36 +0800

    dist

--- pi-client/pi-client/pi_client.py
+++ pi-client/pi-client/pi_client.py
@@ -1,20 +1,38 @@
 from socketIO_client import SocketIO, BaseNamespace
 
 class VaseNamespace(BaseNamespace):
+    # user_type
+    # user_name
+
     def on_connect(self):
         print('[Connected]')
-        #self.emit('login', { 'roomName': 'design-thinking',
-        #                    'userType':'parent',
-        #                    'userName':'daddy' })
+        self.emit('getUserType', 'design-thinking')
+        
+    def on_reconnect(self):
+        self.emit('login', { 'roomName': 'design-thinking',
+                            'userType':self.user_type,
+                            'userName':self.user_name })
+
+    def on_user_type(self, user):
+        if(user):
+            self.user_type = user.user_type
+            self.user_name = user.user_name
 
     def on_test_pi(self, *args):
         print(args)
 
+    def on_message(self, messages):
+        print(data)
+
+    def on_unread_message(self,messages):
+        pass
+
 def main():
     with SocketIO('localhost', 18888, VaseNamespace) as socketIO:
-        socket = socketIO.define(VaseNamespace, '/VASE')
+        vase_socket = socketIO.define(VaseNamespace, '/VASE')
         socketIO.wait(True)
-
+        raw_input_A = raw_input("raw_input: ")
+        vase_socket.emit('sendMessage',raw_input_A)
 
 if __name__ == '__main__':
     main()
