2bcf70dab240a1f049718c115edcd16b843952ef
unHealthy <chenzhutian1992@gmail.com>
2016-07-09 12:18:57 +0800

    restructure the pi-client

--- pi-client/pi_client.py
+++ pi-client/pi_client.py
@@ -1,5 +1,4 @@
 from socketIO_client import SocketIO, BaseNamespace
-
 class VaseNamespace(BaseNamespace):
     # user_type
     # user_name
@@ -27,11 +26,20 @@ class VaseNamespace(BaseNamespace):
     def on_unread_message(self,messages):
         pass
 
+    def login():
+        if(self.userName is None or len(self.userName) == 0):
+            return
+        self.socket.emit('login', {
+            userName: this.userName,
+            roomName: 'design-thinking',
+        })
+
 def main():
     with SocketIO('localhost', 18888, VaseNamespace) as socketIO:
         vase_socket = socketIO.define(VaseNamespace, '/VASE')
         raw_input_A = raw_input("raw_input: ")
         vase_socket.emit('sendMessage',raw_input_A)
+        userName = input
         socketIO.wait()
 
 if __name__ == '__main__':
