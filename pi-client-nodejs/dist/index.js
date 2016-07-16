500e7e1caab0de0da128c3153395c7e4e592fc63
unHealthy <chenzhutian1992@gmail.com>
2016-07-16 14:10:12 +0800

    try to use local host

--- pi-client-nodejs/dist/index.js
+++ pi-client-nodejs/dist/index.js
@@ -3,7 +3,7 @@ const piClent_1 = require('./piClent');
 const args = process.argv.slice(2);
 const userName = args[0];
 const __DEVELOPMENT__ = args[1] === "-production" ? false : true;
-const hostUrl = __DEVELOPMENT__ ? 'http://localhost:18888' : 'http://115.159.30.68';
+const hostUrl = __DEVELOPMENT__ ? 'http://localhost:18888' : 'http://192.168.43.2:18888';
 if (userName !== 'daddy' && userName !== 'boy') {
     throw new Error('wrong user Name!');
 }
--- pi-client-nodejs/src/index.ts
+++ pi-client-nodejs/src/index.ts
@@ -4,7 +4,7 @@ import PiClient from './piClent';
 const args = process.argv.slice(2);
 const userName = args[0];
 const __DEVELOPMENT__ = args[1] === "-production" ? false : true;
-const hostUrl = __DEVELOPMENT__ ? 'http://localhost:18888' : 'http://115.159.30.68';
+const hostUrl = __DEVELOPMENT__ ? 'http://localhost:18888' : 'http://192.168.43.2:18888';//'http://115.159.30.68';
 if (userName !== 'daddy' && userName !== 'boy') {
     throw new Error('wrong user Name!');
 }
